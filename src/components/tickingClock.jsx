cbd5e1d2f9d22752623416e056f8e8b4ce3f6b29
Prabhat Ranjan <prabhat5172992@gmail.com>
2021-11-12 18:11:34 +0530

    changes added for time correction

--- src/components/tickingClock.jsx
+++ src/components/tickingClock.jsx
@@ -1,37 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class TickingClock extends Component {
-    constructor() {
-        super();
-        this.state = {
-            timeVal: new Date().toLocaleTimeString(),
-        }
+  constructor() {
+    super();
+    this.state = {
+      timeVal: new Date().toLocaleTimeString(),
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.updateClock();
+    }, 1000);
+  }
+  updateClock() {
+    let t = new Date().toTimeString().split(" ")[0];
+    let newTime = null;
+    let tArr = t.split(":");
+    let h = t.substr(0, 2);
+
+    if (h >= 12) {
+      newTime =
+        h - 12 < 10
+          ? `0${h - 12}:${tArr[1]}:${tArr[2]} PM`
+          : `${h - 12}:${tArr[1]}:${tArr[2]} PM`;
+    } else {
+      newTime = `${tArr[0]}:${tArr[1]}:${tArr[2]} AM`;
     }
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.updateClock();
-        }, 1000);
-    }
-    updateClock() {
-        let t = new Date().toLocaleTimeString();
-        
-        if(t.substr(0,2) >= 12) {
-            t = t.includes('PM') ? t : t.concat(' PM');
-        } else {
-            t = t.includes('AM') ? t : t.concat(' AM');
-        }
-        this.setState({
-            timeVal: t,
-        })
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-    render() {
-        const { timeVal } = this.state;
-        return (
-            <div className="Time">
-                <p>{timeVal}</p>
-            </div>
-        )
-    }
+
+    this.setState({
+      timeVal: newTime,
+    });
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  render() {
+    const { timeVal } = this.state;
+    return (
+      <div className="Time">
+        <p>{timeVal}</p>
+      </div>
+    );
+  }
 }
