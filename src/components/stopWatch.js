f90aa16618d8206d80b50b5f10cee390cdf4ba65
Prabhat Ranjan <prabhat5172992@gmail.com>
2021-11-17 21:44:48 +0530

    time issue fixed

--- src/components/stopWatch.js
+++ src/components/stopWatch.js
@@ -8,7 +8,8 @@ export class StopWatch extends Component {
       hh: "00",
       mm: "00",
       ss: "00",
-      ms: "000",
+      ms: "0",
+      mss: "00",
       breakups: [],
       prName: 'Pause',
       btn: "pr"
@@ -25,7 +26,8 @@ export class StopWatch extends Component {
         hh: "00",
         mm: "00",
         ss: "00",
-        ms: "000",
+        ms: "0",
+        mss:"00",
         breakups: [],
         prName: 'Pause',
         btn: "pr"
@@ -33,21 +35,21 @@ export class StopWatch extends Component {
   }
 
   start() {
-    this.timer = setInterval(this.updateTimer);
+    this.timer = setInterval(this.updateTimer, 10);
     this.setState({
         btn: "start"
     })
   }
 
   pr() {
-    const { hh, mm, ss, ms, prName, breakups } = this.state;
+    const { hh, mm, ss, ms, mss, prName, breakups } = this.state;
     clearInterval(this.timer);
     if(prName === 'Resume') {
-        this.timer = setInterval(this.updateTimer);
+        this.timer = setInterval(this.updateTimer, 10);
     }
 
     if(prName === 'Pause') {
-        const b = update(breakups, {$push: [`${hh}:${mm}:${ss}:${ms}`]});
+        const b = update(breakups, {$push: [`${hh}:${mm}:${ss}:${ms}${mss}`]});
         this.setState({
             breakups: b
         })
@@ -67,37 +69,66 @@ export class StopWatch extends Component {
   }
 
   updateTimer() {
-    const { ms, ss } = this.state;
-    let milliSec = 0;
+    const { hh, mm, ms, ss, mss } = this.state;
     let msNum = parseInt(ms);
     let ssNum = parseInt(ss);
+    let mssNum = parseInt(mss);
+    let mmNum = parseInt(mm);
+    let hhNum = parseInt(hh);;
 
-    if (msNum < 10) {
-      milliSec = `00${msNum + 1}`;
-    } else if (msNum < 100) {
-      milliSec = `0${msNum + 1}`;
+    if (mssNum < 9) {
+      mssNum = `0${mssNum + 1}`;
+    } else if(mssNum < 99){
+      mssNum += 1;
+      if(mssNum%10 === 0) {
+        msNum = msNum < 9 ? msNum + 1 : 0;
+      }
     } else {
-      milliSec = msNum + 1;
+        if(ssNum < 59) {
+            ssNum += 1;
+        } else {
+            if(mmNum < 59) {
+                mmNum += 1;
+            } else {
+                hhNum += 1;
+                mmNum = "00";
+            }
+            ssNum = "00";
+        }
+        mssNum = "00";
     }
 
-    if(milliSec > 999 && ssNum < 60) {
-        ssNum = (ssNum < 10) ? `0${ss+1}` : (ss+1);
-        milliSec = '000';
+    if(ssNum < 10) {
+        ssNum = `0${ssNum}`;
     }
+    if(mmNum < 10) {
+        mmNum = `0${mmNum}`;
+    }
+    if(hhNum < 10) {
+        hhNum = `0${hhNum}`;
+    }
+
     this.setState({
-        ms: milliSec,
+        ms: msNum,
         ss: ssNum,
+        mss: mssNum,
+        hh: hhNum,
+        mm: mmNum,
     })
   }
 
   render() {
-    const { hh, mm, ss, ms, prName, btn, breakups } = this.state;
+    const { hh, mm, ss, ms, mss, prName, btn, breakups } = this.state;
 
     return (
       <div>
         <h1>Stop Watch</h1>
         <div className="Time">
-            <p>{`${hh}:${mm}:${ss}:${ms}`}</p>
+            <span>{`${hh}:`}</span>
+            <span>{`${mm}:`}</span>
+            <span>{`${ss}.`}</span>
+            <span>{`${ms}`}</span>
+            <span className="break">{`${mss}`}</span>
         </div>
         <button onClick={this.start} disabled={btn==='start'}>Start</button>
         <button className="pr" onClick={this.pr} disabled={btn==='pr'}>{prName}</button>
